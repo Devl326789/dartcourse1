014acf54b872a5a70ce3738338d51b60b8533a3c
Devl326789 <Devl987623@gmail.com>
2021-02-01 19:26:59 -0800

    launch things

--- lib/0402-if-else.dart
+++ lib/0402-if-else.dart
@@ -3,13 +3,11 @@
 void main() {
   const netSalary = 500;
   const expenses = 376;
-  const saved = (netSalary - expenses);
-  const lost = (expenses - netSalary);
 
   if (netSalary > expenses) {
-    print('You have saved \$$saved this month');
+    print('You have saved \$${netSalary - expenses} this month');
   } else if (expenses > netSalary) {
-    print('You have lost \$$lost this month');
+    print('You have lost \$${expenses - netSalary} this month');
   } else {
     print("Your balance hasn\'t changed");
   }
