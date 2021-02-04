ebbe4c6d81a3223867ed6bb4ea05a198deca72f5
Devl326789 <Devl987623@gmail.com>
2021-02-03 20:12:14 -0800

    fizz bizz 2.0, break and continue

--- lib/0403-while-for.dart
+++ lib/0403-while-for.dart
@@ -1,14 +1,20 @@
-// 04.05 Fizz Bizz exercise
+// 04.05.2 Fizz Bizz exercise
 void main() {
-  for (var i = 1; i < 16; i++) {
+  for (var i = 1; i <= 16; i++) {
     if ((i % 3 == 0) && (i % 5 == 00)) {
       print('Fizz Bizz');
-    } else if (i % 3 == 0) {
+      break;
+    }
+    if (i % 3 == 0) {
       print('Fizz');
-    } else if (i % 5 == 0) {
+      continue;
+    }
+    if (i % 5 == 0) {
       print('Bizz');
     } else {
       print(i);
+      continue;
     }
   }
+  print('done');
 }
