bafcef1d9b192fc8aa7522a99588d4a5bd66bff6
Devl326789 <Devl987623@gmail.com>
2021-02-03 20:17:41 -0800

    Switch statements

---
+++ lib/switches.dart
@@ -0,0 +1,17 @@
+void main() {
+  const pos = 1;
+  switch (pos) {
+    case 1:
+      print('Gold');
+      break;
+    case 2:
+      print('Silver');
+      break;
+    case 3:
+      print('Bronze');
+      break;
+    default:
+      print('No medal');
+      break;
+  }
+}
