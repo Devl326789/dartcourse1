c8b17f38576a1368215a552ddb6c037ae7efb1e5
Devl326789 <Devl987623@gmail.com>
2021-02-06 08:25:04 -0800

    using collection-if

---
+++ lib/5/collection-if.dart
@@ -0,0 +1,21 @@
+// Using collection-if to make it easier to add to colections
+
+void main() {
+  final colors = ['grey', 'brown'];
+  const addBlue = false;
+  const addRed = true;
+  if (addBlue) {
+    colors.add('blue');
+  }
+  if (addRed) {
+    colors.add('red');
+  }
+  final colors2 = [
+    'grey',
+    'brown',
+    if (!addBlue) 'blue',
+    if (!addRed) 'red',
+  ];
+  print(colors);
+  print(colors2);
+}
