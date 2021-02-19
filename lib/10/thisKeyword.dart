d9460ce6c31ad01d042d61fa44eb42c22926bcbd
Devl326789 <Devl987623@gmail.com>
2021-02-18 18:18:56 -0800

    Initializers and shorthand Constructors

---
+++ lib/10/thisKeyword.dart
@@ -0,0 +1,28 @@
+class BankAccount {
+  BankAccount({
+    this.accountHolder,
+    this.balance = 0,
+  });
+  String accountHolder;
+  double balance;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+// returning a bool is a poor method of error handling
+// We should use exceptions
+  bool withdrawl(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+void main() {
+  final bankAccount = BankAccount(accountHolder: 'bob', balance: 100);
+  print(bankAccount.balance);
+}
