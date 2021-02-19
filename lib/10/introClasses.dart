c532cf466dd59937caa79cb79c54dcaab24ca365
Devl326789 <Devl987623@gmail.com>
2021-02-18 18:03:05 -0800

    instance methods

--- lib/10/introClasses.dart
+++ lib/10/introClasses.dart
@@ -6,8 +6,4 @@ void main() {
   final bankAccount = BankAccount();
   bankAccount.balance = 100;
   print(bankAccount.balance);
-
-  final BankAccount2 = BankAccount();
-  BankAccount2.balance = 500;
-  print(BankAccount2.balance);
 }
