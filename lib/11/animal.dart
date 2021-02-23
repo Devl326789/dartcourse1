8a085fe59b384a93c8c151aabbe21647c79a49f3
Devl326789 <Devl987623@gmail.com>
2021-02-22 19:41:29 -0800

    The super constructor

--- lib/11/animal.dart
+++ lib/11/animal.dart
@@ -1,30 +1,40 @@
+//  Constructors are not inherited in Dart
 class Animal {
+  const Animal({required this.age});
+  final int age;
+
   void sleep() => print('Sleep');
 }
 
 class Dog extends Animal {
+  Dog({required int age}) : super(age: age);
+
   void bark() => print('Bark');
 }
 
 class Cow extends Animal {
+  Cow({required int age}) : super(age: age);
+
   void moo() => print('Moo');
 }
 
 class CleverDog extends Dog {
+  CleverDog({required int age}) : super(age: age);
+
   void catchBall() => print('Catch ball');
 }
 
 void main() {
-  final animal = Animal();
+  final animal = Animal(age: 10);
   animal.sleep();
 
-  final dog = Dog();
+  final dog = Dog(age: 10);
   dog.bark();
   dog.sleep();
 
-  final cow = Cow();
+  final cow = Cow(age: 10);
   cow.moo();
 
-  final cleverDog = CleverDog();
+  final cleverDog = CleverDog(age: 10);
   cleverDog.catchBall();
 }
